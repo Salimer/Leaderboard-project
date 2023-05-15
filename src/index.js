6cad8968f9d96a7aa13fe21c6cd345124d98b722
Salimer <52242629+Salimer@users.noreply.github.com>
2023-05-15 18:06:51 +0300

    Add scores dynamically

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './style.scss';
+import leaderBoard from './modules/leaderBoard';
+import listScores from './modules/listScores'
+
+listScores(leaderBoard.scoresList);
+
+const submitBtn = document.querySelector('#submit');
+submitBtn.addEventListener('click', () => {
+    const newName = document.querySelector('#new-name');
+    const newScore = document.querySelector('#new-score');
+    if (newName.value !== '' && newScore.value !== '') {
+        leaderBoard.addItem(newName.value, newScore.value);
+    } else {
+        alert('Please fill both fields');
+    }
+})
