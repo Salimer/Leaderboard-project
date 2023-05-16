984e3c12b7b17c755e0e35c261f035e9e39cb354
Salimer <52242629+Salimer@users.noreply.github.com>
2023-05-16 15:37:32 +0300

    Add postNewScore functionality to the submit button

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import './style.scss';
-import listScores from './modules/listScores.js';
+import Leaderboard from './modules/LeaderBoard';
 import init from './modules/init.js';
+import listScores from './modules/listScores.js';
+import postNewScore from './modules/postNewScore.js';
 
 // Starting a new leader board
-init();
+const newLB = new Leaderboard();
+const gameID = 'yVWfxt83ZzUTJiUF6itj'
+init(newLB, gameID);
+
+// Listen to Refresh button
+const refresh = document.querySelector('#refresh');
+refresh.addEventListener('click', () => {
+  init(newLB, gameID);
+})
 
 // Listen to the submit button
 const submitBtn = document.querySelector('#submit');
@@ -12,9 +22,9 @@ submitBtn.addEventListener('click', () => {
   const newScore = document.querySelector('#new-score');
   if (newName.value !== '' && newScore.value !== '') {
     newLB.addItem(newName.value, newScore.value);
+    postNewScore(gameID, newName.value, newScore.value);
     newName.value = '';
     newScore.value = '';
-    listScores(newLB.list);
   } else {
     // eslint-disable-next-line no-alert
     alert('Please fill both fields');
