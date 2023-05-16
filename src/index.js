a10206a6063587b3a1d9eb1c23e4f688869cde2f
Salimer <52242629+Salimer@users.noreply.github.com>
2023-05-16 16:58:16 +0300

    Remove everything from index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,5 @@
 import './style.scss';
-import Leaderboard from './modules/LeaderBoard';
-import init from './modules/init.js';
-import listScores from './modules/listScores.js';
-import postNewScore from './modules/postNewScore.js';
+import setup from './modules/setup';
 
 // Starting a new leader board
-const newLB = new Leaderboard();
-const gameID = 'yVWfxt83ZzUTJiUF6itj'
-init(newLB, gameID);
-
-// Listen to Refresh button
-const refresh = document.querySelector('#refresh');
-refresh.addEventListener('click', () => {
-  init(newLB, gameID);
-})
-
-// Listen to the submit button
-const submitBtn = document.querySelector('#submit');
-submitBtn.addEventListener('click', () => {
-  const newName = document.querySelector('#new-name');
-  const newScore = document.querySelector('#new-score');
-  if (newName.value !== '' && newScore.value !== '') {
-    newLB.addItem(newName.value, newScore.value);
-    postNewScore(gameID, newName.value, newScore.value);
-    newName.value = '';
-    newScore.value = '';
-  } else {
-    // eslint-disable-next-line no-alert
-    alert('Please fill both fields');
-  }
-});
+setup();
