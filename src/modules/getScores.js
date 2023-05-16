4b102e96f0f3e78a11f46dce10775c7ad144c0b4
Salimer <52242629+Salimer@users.noreply.github.com>
2023-05-16 17:00:02 +0300

    Remove getScores.js

--- src/modules/getScores.js
+++
@@ -1,9 +0,0 @@
-export default async (ID = 'yVWfxt83ZzUTJiUF6itj') => {
-    const requestURL = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${ID}/scores`;
-    const request = new Request(requestURL);
-  
-    const response = await fetch(request);
-    const superHeroes = await response.json();
-    
-    return superHeroes.result;
-  }
