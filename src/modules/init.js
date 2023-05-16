c7fb842e69339bb4dee011e5b33eb4e1f61f9470
Salimer <52242629+Salimer@users.noreply.github.com>
2023-05-16 15:37:44 +0300

    Modify init.js

--- src/modules/init.js
+++ src/modules/init.js
@@ -1,11 +1,7 @@
-import LeaderBoard from "./leaderBoard.js";
 import getScores from "./getScores.js";
 import listScores from "./listScores.js";
 
-export default async () => {
-    const newLB = new LeaderBoard();
-  
-    newLB.list = await getScores('yVWfxt83ZzUTJiUF6itj');
-    console.log(newLB.list);
+export default async (newLB, gameID) => {  
+    newLB.list = await getScores(gameID);
     listScores(newLB.list);
 };
