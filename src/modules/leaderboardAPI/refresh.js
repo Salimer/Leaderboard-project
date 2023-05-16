8c76cb6a6ecb9f06136f10e850b52153a9f95b3b
Salimer <52242629+Salimer@users.noreply.github.com>
2023-05-16 17:46:46 +0300

    Modify refresh.js

--- src/modules/leaderboardAPI/refresh.js
+++ src/modules/leaderboardAPI/refresh.js
@@ -1,7 +1,7 @@
-import getScores from "./getScores.js";
-import listScores from "./listScores.js";
+import getScores from './getScores.js';
+import listScores from './listScores.js';
 
-export default async (newLB, gameID) => {  
-    newLB.list = await getScores(gameID);
-    listScores(newLB.list);
+export default async (newLB, gameID) => {
+  newLB.list = await getScores(gameID);
+  listScores(newLB.list);
 };
