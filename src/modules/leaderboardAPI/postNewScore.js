a85258069aacad59f168d50cc56cea3d525cbc15
Salimer <52242629+Salimer@users.noreply.github.com>
2023-05-16 17:47:08 +0300

    Update postNewScore module

--- src/modules/leaderboardAPI/postNewScore.js
+++ src/modules/leaderboardAPI/postNewScore.js
@@ -1,18 +1,16 @@
 export default async (ID, newUser, newScore) => {
-    const requestURL = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${ID}/scores`;
-    const obj = {
-        user: newUser,
-        score: `${newScore}`
-    };
-  
-    const request = new Request(requestURL, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(obj)
-    });
-    const response = await fetch(request);
-    const responceMsg = await response.json();
-    console.log(responceMsg);
-  }
+  const requestURL = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${ID}/scores`;
+  const obj = {
+    user: newUser,
+    score: `${newScore}`,
+  };
+
+  const request = new Request(requestURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(obj),
+  });
+  await fetch(request);
+};
