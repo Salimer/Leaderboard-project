5e3aabc06d98de9b0b26b2cc3bec3d4903bd5c70
Salimer <52242629+Salimer@users.noreply.github.com>
2023-05-16 15:08:00 +0300

    Use for..of instead of forEach in listScores module

--- src/modules/listScores.js
+++ src/modules/listScores.js
@@ -1,14 +1,18 @@
-export default (list) => {
-  const container = document.querySelector('.scores-container');
-  // Reset score items
-  while (container.children[0]) {
-    container.removeChild(container.children[0]);
-  }
+export default async (list) => {
+  try {
+    const container = document.querySelector('.scores-container');
+    // Reset score items
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
 
-  list.forEach((item) => {
-    const scoreItem = document.createElement('li');
-    scoreItem.className = 'score-item';
-    scoreItem.innerHTML = `${item.name}: ${item.score}`;
-    container.appendChild(scoreItem);
-  });
+    for (const item of list) {
+      const scoreItem = document.createElement('li');
+      scoreItem.className = 'score-item';
+      scoreItem.textContent = `${item.user}: ${item.score}`;
+      container.appendChild(scoreItem);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
 };
