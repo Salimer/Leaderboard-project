231100dc9f5fa4e0e4c6deb3d8971d55b1ef3c37
Salimer <52242629+Salimer@users.noreply.github.com>
2023-05-16 17:46:08 +0300

    Add init module

--- src/modules/init.js
+++ src/modules/init.js
@@ -2,25 +2,25 @@ import refresh from './leaderboardAPI/refresh.js';
 import postNewScore from './leaderboardAPI/postNewScore.js';
 
 export default (newLB, gameID) => {
-    // Listen to Refresh button
-    const refreshBtn = document.querySelector('#refresh');
-    refreshBtn.addEventListener('click', () => {
-        refresh(newLB, gameID);
-    })
+  // Listen to Refresh button
+  const refreshBtn = document.querySelector('#refresh');
+  refreshBtn.addEventListener('click', () => {
+    refresh(newLB, gameID);
+  });
 
-    // Listen to the submit button
-    const submitBtn = document.querySelector('#submit');
-    submitBtn.addEventListener('click', () => {
-        const newName = document.querySelector('#new-name');
-        const newScore = document.querySelector('#new-score');
-        if (newName.value !== '' && newScore.value !== '') {
-            newLB.addItem(newName.value, newScore.value);
-            postNewScore(gameID, newName.value, newScore.value);
-            newName.value = '';
-            newScore.value = '';
-        } else {
-            // eslint-disable-next-line no-alert
-            alert('Please fill both fields');
-        }
-    });
-}
+  // Listen to the submit button
+  const submitBtn = document.querySelector('#submit');
+  submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const newName = document.querySelector('#new-name');
+    const newScore = document.querySelector('#new-score');
+    if (newName.value !== '' && newScore.value !== '') {
+      postNewScore(gameID, newName.value, newScore.value);
+      newName.value = '';
+      newScore.value = '';
+    } else {
+      // eslint-disable-next-line no-alert
+      alert('Please fill both fields');
+    }
+  });
+};
