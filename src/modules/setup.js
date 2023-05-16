4b47195277659bc59e2e64675df6ed5951b44529
Salimer <52242629+Salimer@users.noreply.github.com>
2023-05-16 17:46:52 +0300

    Modify setup.js

--- src/modules/setup.js
+++ src/modules/setup.js
@@ -1,12 +1,9 @@
-import LeaderBoard from "./LeaderBoard.js";
-import { setLeaderboardID } from "./idManager.js";
-import init from "./init.js";
-import newGameID from "./leaderboardAPI/newGameID.js";
+import LeaderBoard from './LeaderBoard.js';
+import init from './init.js';
+import newGameID from './leaderboardAPI/newGameID.js';
 
-
-export default async () => {  
-    const newLB = new LeaderBoard();
-    const gameID = await newGameID();
-    setLeaderboardID(gameID);
-    init(newLB, gameID);
+export default async () => {
+  const newLB = new LeaderBoard();
+  const gameID = await newGameID();
+  init(newLB, gameID);
 };
